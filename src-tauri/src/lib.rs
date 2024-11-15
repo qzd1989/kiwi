21426d30b873cf010d6d9a6996ab840001c6916d
kiwi <qzd1989@gmail.com>
2024-11-15 18:39:07 +0800

    1

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,10 +5,6 @@ pub mod find;
 pub mod input;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -20,7 +16,6 @@ pub fn run() {
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![
-            greet,
             commands::fs::create_dir,
             commands::fs::create_file,
             commands::fs::read_dir,
@@ -41,3 +36,17 @@ pub fn run() {
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
+
+// use pyo3::prelude::*;
+
+// #[pyfunction]
+// fn guess_the_number() -> PyResult<i32> {
+//     println!("Guess the number!");
+//     Ok(5)
+// }
+
+// #[pymodule]
+// fn guessing_game(m: &Bound<'_, PyModule>) -> PyResult<()> {
+//     m.add_function(wrap_pyfunction!(guess_the_number, m)?)?;
+//     Ok(())
+// }
