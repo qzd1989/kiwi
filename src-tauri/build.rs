5b9a880580276add94a1f67ab8c0f90cf635f5b8
kiwi <qzd1989@gmail.com>
2024-11-16 21:38:37 +0800

    1

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,5 @@
 fn main() {
+    println!("cargo:rustc-link-search=native=C:\\tools\\opencv\\build\\x64\\vc15\\lib");
+    println!("cargo:rustc-link-lib=static=opencv_world455");
     tauri_build::build()
 }
